90e542404a0ed9f4812e3dcfb439ea0852fa58da
tfkhdyt <tfkhdyt@proton.me>
2024-06-13 09:33:45 +0700

    fix(validator/supplier): add unique to email and phone field

--- app/validators/supplier.ts
+++ app/validators/supplier.ts
@@ -36,11 +36,37 @@ export const createSupplierValidator = vine.compile(
  * Validator to validate the payload when updating
  * an existing supplier.
  */
-export const updateSupplierValidator = vine.compile(
+export const updateSupplierValidator = vine.withMetaData<{ supplierId: number }>().compile(
   vine.object({
     name: vine.string().trim().maxLength(100).optional(),
-    email: vine.string().trim().email().maxLength(50).optional(),
-    phone: vine.string().trim().maxLength(30).nullable().optional(),
+    email: vine
+      .string()
+      .trim()
+      .email()
+      .maxLength(50)
+      .unique(async (_, value, field) => {
+        const supplier = await Supplier.query()
+          .whereNot('id', field.meta.supplierId)
+          .where('email', value)
+          .first()
+
+        return !supplier
+      })
+      .optional(),
+    phone: vine
+      .string()
+      .trim()
+      .maxLength(30)
+      .unique(async (_, value, field) => {
+        const supplier = await Supplier.query()
+          .whereNot('id', field.meta.supplierId)
+          .where('phone', value)
+          .first()
+
+        return !supplier
+      })
+      .nullable()
+      .optional(),
     address: vine.string().trim().nullable().optional(),
   })
 )
